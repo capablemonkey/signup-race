39941ae489b6c0d9a4a74eb771dd7ace0d401363
Gordon Zheng <technix1@gmail.com>
2015-04-30 17:43:37 -0400

    take more money away

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,13 +11,13 @@ $( document ).ready(function() {
     
     var w = 550;
     var h = 550;
-    var left = (screen.width/2)-(w/2);
+    var left = (screen.width/2);
     var top = (screen.height/2)-(h/2);
 
     popup = window.open($(this).data('url'), 'Dwolla OAuth', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
 
     moneyInterval = setInterval(function() {
-      money -= .83;
+      money -= 3.83;
       $('#moneyBox').html('$' + money.toFixed(2));
     }, 100);
 
